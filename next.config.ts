58c6dd4a2216cc46deade7badb2e3de839bb1058
rroscian <150047837+rroscian@users.noreply.github.com>
2025-09-08 01:52:25 +0200

    Réimplemente configuration standalone
    Tests de perfs et de stabilité sans standalone, réimplémentation

--- next.config.ts
+++ next.config.ts
@@ -4,24 +4,8 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   compress: true,
   poweredByHeader: false,
-  serverExternalPackages: ['prisma', '@prisma/client'],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('_http_common');
-    }
-    return config;
-  },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'teamup-rroscian.onrender.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
+    domains: ['teamup-rroscian.onrender.com', 'images.unsplash.com'],
   },
 };
 
